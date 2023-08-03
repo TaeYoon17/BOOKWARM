15dca2e8c57bbab8dc6509ca69c5902187955d04
TaeYoon17 <kim0517@hotmail.com>
2023-08-03 20:45:09 +0900

    Thursday_0803

--- BOOKWARM/DetailGroup/DetailVC.swift
+++ BOOKWARM/DetailGroup/DetailVC.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class DetailVC: UIViewController,MovieObserver{
-    enum SegmentType:Int{
-        case Overview, Memo
-    }
+    enum SegmentType:Int{ case Overview, Memo }
     enum SegueType:String{ case Push = "chevron.left",Modally = "xmark" }
     static let identifier = "DetailVC"
     var movieSubscriber: ((Movie)->())?
@@ -20,8 +18,14 @@ class DetailVC: UIViewController,MovieObserver{
             movieSubscriber(movie)
         }
     }
+    var memo:String?{
+        didSet{
+            print(memo)
+        }
+    }
     var headerBg: UIColor?
     var segueType: SegueType = .Push
+    var placeHolder = "메모를 입력하세요"
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var playTimeLabel: UILabel!
@@ -80,8 +84,13 @@ class DetailVC: UIViewController,MovieObserver{
         self.navigationController?.navigationBar.scrollEdgeAppearance?.backgroundColor = headerBg
         self.headerBgView.backgroundColor = headerBg
     }
+    @IBAction func rootViewTapped(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+        self.memoTextView.resignFirstResponder()
+    }
     @objc func heartBtnTapped(){
         self.like.toggle()
+        self.view.endEditing(true)
     }
     @objc func popVC(){
         switch segueType {
@@ -93,9 +102,11 @@ class DetailVC: UIViewController,MovieObserver{
     }
     @IBAction func segmentTapped(_ sender: UISegmentedControl) {
         guard let type = SegmentType(rawValue: sender.selectedSegmentIndex) else {return}
+        self.view.endEditing(true)
         self.segmentType = type
     }
 }
+//MARK: -- VC 초기 세팅
 extension DetailVC{
     func configure(){
         guard let movie else {return}
@@ -106,13 +117,62 @@ extension DetailVC{
         imageView.image = .init(named: movie.title)
         overviewLabel.text = movie.overview
         self.segmentType = .Overview
+        self.memoTextView.delegate = self
+        self.memoTextView.font = .systemFont(ofSize: 20, weight: .medium)
+        if let memo, memo.trimmingPrefix([" ","\n"]) != ""{
+            self.memoTextView.set(valueType: .Exist(value: memo))
+        }else{
+            self.memoTextView.set(valueType: .Empty(placeholder: self.placeHolder))
+        }
     }
 }
-
+extension DetailVC:UITextViewDelegate{
+    func textViewDidEndEditing(_ textView: UITextView) {
+        let trimmedText = textView.text.trimmingCharacters(in: [" ","\n"])
+        self.memo = trimmedText
+        textView.set(stateType: .End(placeholder: self.placeHolder))
+        textView.resignFirstResponder()
+    }
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.set(stateType: .Begin(placeholder: self.placeHolder))
+    }
+}
 fileprivate extension UIBarButtonItem{
     func setLike(like:Bool){
         print(#function)
         self.image = .init(systemName: "heart\(like ? ".fill":"")")
         self.tintColor = like ? .red : .gray
+    }
+}
+fileprivate extension UITextView{
+    enum StateType{
+        case Begin(placeholder: String)
+        case End(placeholder: String)
+    }
+    enum ValueType{
+        case Empty(placeholder: String)
+        case Exist(value:String)
+    }
+    private func set(text:String,color:UIColor){
+        self.textColor = color
+        self.text = text
+    }
+    func set(valueType:ValueType){
+        switch valueType{
+        case .Empty(let p): self.set(text: p, color: .lightGray)
+        case .Exist(let v): self.set(text: v,color:.black)
+        }
+    }
+    func set(stateType:StateType){
+        switch stateType{
+        case .Begin(let placeholder):
+            if self.text == placeholder{
+                self.set(valueType: .Exist(value: ""))
+            }
+        case .End(let placeholder):
+            if self.text.trimmingCharacters(in: [" ","\n"]) == ""{
+                self.set(valueType: .Empty(placeholder: placeholder))
+            }
+        }
     }
 }
